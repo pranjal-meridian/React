b3151466e9c052fc3d134360d4c1ea701d5be713
pranjal-meridian <pranjal.raghuvanshi@onmeridian.com>
2025-04-06 23:29:56 +0530

    added logs page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Register from "./Register.jsx";
 import Challenge from "./Challenge.jsx";
 import Login from "./Login.jsx";
-import Dashboard from './components/Dashboard';
+import Logs from "./Logs";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Register />} />
         <Route path="/challenge" element={<Challenge />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path="/logs" element={<Logs />} />
       </Routes>
     </Router>
   )
